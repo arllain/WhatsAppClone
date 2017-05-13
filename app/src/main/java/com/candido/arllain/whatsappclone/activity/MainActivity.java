316a06cc83541cb3374475ef0decc8efc3c31302
arllain <arllain@gmail.com>
2017-05-12 22:31:21 -0300

    commit

--- app/src/main/java/com/candido/arllain/whatsappclone/activity/MainActivity.java
+++ app/src/main/java/com/candido/arllain/whatsappclone/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.candido.arllain.whatsappclone;
+package com.candido.arllain.whatsappclone.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.candido.arllain.whatsappclone.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/candido/arllain/whatsappclone/activity/ValidadorActivity.java
@@ -0,0 +1,15 @@
+package com.candido.arllain.whatsappclone.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.candido.arllain.whatsappclone.R;
+
+public class ValidadorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_validador);
+    }
+}
